305c64ed93a952bf378d6c17a1e435a31a4db303
MasseyIsaako <lasiisaako@gmail.com>
2020-07-07 12:22:54 +1000

    chat: notify users when someone has left, added emojify.

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -8,13 +8,13 @@ import './InfoBar.css';
 const InfoBar = ({ room }) => (
     <div className="infoBar">
         <div className="leftInnerContainer">
-            <img className="onlineIcon" src={onlineIcon} alt="Online image" />
+            <img className="onlineIcon" src={onlineIcon} alt="Online" />
             <h3>{ room }</h3>
         </div>
 
         <div className="rightInnerContainer">
             <a href="/">
-                <img src={closeIcon} alt="Close image" />
+                <img src={closeIcon} alt="Close" />
             </a>
         </div>
     </div>
